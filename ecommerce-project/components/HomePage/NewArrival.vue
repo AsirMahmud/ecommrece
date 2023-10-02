560ac01348e1618a8ea4b56692d8f36710bab581
AsirMahmud <email@asirmahmuhhdd@gamil.com>
2023-10-02 09:52:22 +0600

    df

--- ecommerce-project/components/HomePage/NewArrival.vue
+++ ecommerce-project/components/HomePage/NewArrival.vue
@@ -1,6 +1,6 @@
 <script setup>
 const { data, pending, error } = await useFetch(
-  `https://fakestoreapi.com/products?limit=${windowChanger()}`
+  "https://fakestoreapi.com/products?limit=2"d
 );
 </script>
 
